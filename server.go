626c6d4ed460963ae453970d9fd7b4dbd382a456
eugeniusms <eugeniusms@gmail.com>
2023-02-16 00:22:56 +0700

    🛠 Add gin.log

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/eugeniusms/poseidon/controller"
 	"github.com/eugeniusms/poseidon/middlewares"
 	"github.com/eugeniusms/poseidon/service"
@@ -12,7 +15,15 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
+	
+	setupLogOutput()
+
 	server := gin.Default()
 
 	server.Use(gin.Recovery(), middlewares.Logger())
