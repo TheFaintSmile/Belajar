3a65cd2d95bc2e0a7022a39107b6107dd2d37ed2
iqbalpa <iqbalpahlevi16@gmail.com>
2023-08-25 08:37:30 +0700

    fix: set email field in registration unique

--- internal/app/entity/user.go
+++ internal/app/entity/user.go
@@ -12,7 +12,7 @@ type User struct {
 	FirstName string `json:"firstname" binding:"required"`
 	LastName  string `json:"lastname" binding:"required"`
 	Age       int8   `json:"age" binding:"gte=1,lte=130"`
-	Email     string `json:"email" binding:"required,email"`
+	Email     string `json:"email" binding:"required,email" gorm:"unique"`
 	Password  string `json:"password" binding:"required,min=6"`
 }
 
