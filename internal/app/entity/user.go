93c5a6b3e23c481c92dfe4341d25481a9d21f92f
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-09-12 22:55:21 +0700

    feat: add user level (jenjang)

--- internal/app/entity/user.go
+++ internal/app/entity/user.go
@@ -9,13 +9,27 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+const (
+	LevelSD1      UserLevel = "SD-1"
+	LevelSD2      UserLevel = "SD-2"
+	LevelSD3 	  UserLevel = "SD-3"
+	LevelSD4      UserLevel = "SD-4"
+	LevelSD5      UserLevel = "SD-5"
+	LevelSD6      UserLevel = "SD-6"
+	LevelSMP      UserLevel = "SMP"
+	LevelSMA      UserLevel = "SMA"
+)
+
+type UserLevel string
+
 type User struct {
 	gorm.Model
-	FirstName string `json:"firstname" binding:"required"`
-	LastName  string `json:"lastname" binding:"required"`
-	Age       int8   `json:"age" binding:"gte=1,lte=130"`
-	Email     string `json:"email" binding:"required" gorm:"unique"`
-	Password  string `json:"password" binding:"required,min=6"`
+	FirstName string 	`json:"firstname" binding:"required"`
+	LastName  string 	`json:"lastname" binding:"required"`
+	Age       int8   	`json:"age" binding:"gte=1,lte=130"`
+	Email     string 	`json:"email" binding:"required" gorm:"unique"`
+	Password  string 	`json:"password" binding:"required,min=6"`
+	Level	  UserLevel `json:"level" binding:"required"`
 }
 
 func (u *User) SaveUser() (*User, error) {
