519d8488ec6c239370c0301f5e13670afc3b3ec7
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-09-12 23:58:47 +0700

    refactor: response handler status & message

--- internal/app/routes/auth.go
+++ internal/app/routes/auth.go
@@ -5,27 +5,12 @@ import (
 	"github.com/jinzhu/gorm"
 	controller "github.com/rumbel/belajar/internal/app/controller/auth"
 	"github.com/rumbel/belajar/internal/app/service"
+	utils "github.com/rumbel/belajar/internal/app/utils"
 )
 
 var (
 	authService service.AuthService = service.NewAuthService()
 )
-
-// Response is a custom struct to format responses consistently
-type Response struct {
-	Message string      `json:"message"`
-	Status  int         `json:"status"`
-	Data    interface{} `json:"data"`
-}
-
-// sendResponse sends a response with a standardized format
-func sendResponse(ctx *gin.Context, status int, message string, data interface{}) {
-	ctx.JSON(status, Response{
-		Message: message,
-		Status:  status,
-		Data:    data,
-	})
-}
 
 func AuthRoutes(api *gin.RouterGroup, db *gorm.DB) {
 	authController := controller.NewAuthController(authService, db)
@@ -34,25 +19,25 @@ func AuthRoutes(api *gin.RouterGroup, db *gorm.DB) {
 	{
 		// test endpoint
 		auth.GET("/", func(ctx *gin.Context) {
-			sendResponse(ctx, 200, "This is the auth endpoint", nil)
+			utils.SendResponse(ctx, 200, "This is the auth endpoint", nil)
 		})
 		// register endpoint
 		auth.POST("/register", func(ctx *gin.Context) {
 			err := authController.Register(ctx)
 			if err != nil {
-				sendResponse(ctx, 400, err.Error(), nil)
+				utils.SendResponse(ctx, 400, err.Error(), nil)
 				return
 			}
-			sendResponse(ctx, 200, "Registration success", nil)
+			utils.SendResponse(ctx, 200, "Registration success", nil)
 		})
 		// login endpoint
 		auth.POST("/login", func(ctx *gin.Context) {
 			token, err := authController.Login(ctx)
 			if err != nil {
-				sendResponse(ctx, 400, err.Error(), nil)
+				utils.SendResponse(ctx, 400, err.Error(), nil)
 				return
 			}
-			sendResponse(ctx, 200, "Login success", gin.H{"token": token})
+			utils.SendResponse(ctx, 200, "Login success", gin.H{"token": token})
 		})
 	}
 }
--- internal/app/utils/handler.go
+++ internal/app/utils/handler.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Response is a custom struct to format responses consistently
+type Response struct {
+	Message string      `json:"message"`
+	Status  int         `json:"status"`
+	Data    interface{} `json:"data"`
+}
+
+// sendResponse sends a response with a standardized format
+func SendResponse(ctx *gin.Context, status int, message string, data interface{}) {
+	ctx.JSON(status, Response{
+		Message: message,
+		Status:  status,
+		Data:    data,
+	})
+}
