0b093a09a8b6c3c8abd64edd8769c898ca5b8ef9
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-09-12 23:50:19 +0700

    feat: send response default handler

--- internal/app/routes/auth.go
+++ internal/app/routes/auth.go
@@ -8,46 +8,51 @@ import (
 )
 
 var (
-    authService    service.AuthService    = service.NewAuthService()
+	authService service.AuthService = service.NewAuthService()
 )
+
+// Response is a custom struct to format responses consistently
+type Response struct {
+	Message string      `json:"message"`
+	Status  int         `json:"status"`
+	Data    interface{} `json:"data"`
+}
+
+// sendResponse sends a response with a standardized format
+func sendResponse(ctx *gin.Context, status int, message string, data interface{}) {
+	ctx.JSON(status, Response{
+		Message: message,
+		Status:  status,
+		Data:    data,
+	})
+}
 
 func AuthRoutes(api *gin.RouterGroup, db *gorm.DB) {
 	authController := controller.NewAuthController(authService, db)
-	
+
 	auth := api.Group("/auth")
 	{
 		// test endpoint
 		auth.GET("/", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{
-				"message": "this is auth endpoint",
-			})
+			sendResponse(ctx, 200, "This is the auth endpoint", nil)
 		})
 		// register endpoint
 		auth.POST("/register", func(ctx *gin.Context) {
 			err := authController.Register(ctx)
 			if err != nil {
-				ctx.JSON(400, gin.H{
-					"error": err.Error(),
-				})
+				sendResponse(ctx, 400, err.Error(), nil)
 				return
 			}
-			ctx.JSON(200, gin.H{
-				"message": "registration success",
-			})
+			sendResponse(ctx, 200, "Registration success", nil)
 		})
 		// login endpoint
 		auth.POST("/login", func(ctx *gin.Context) {
 			token, err := authController.Login(ctx)
 			if err != nil {
-				ctx.JSON(400, gin.H{
-					"error": err.Error(),
-				})
+				sendResponse(ctx, 400, err.Error(), nil)
 				return
 			}
-			ctx.JSON(200, gin.H{
-				"message": "login success",
-				"token": token,
-			})
+			sendResponse(ctx, 200, "Login success", gin.H{"token": token})
 		})
 	}
 }
