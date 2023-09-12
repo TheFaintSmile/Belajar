6b710393132d88f2911f67a131f1f9df9811cf68
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-09-12 23:21:36 +0700

    update: swagger docs

--- cmd/server/docs/docs.go
+++ cmd/server/docs/docs.go
@@ -19,12 +19,12 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
-	Host:             "localhost:8080",
-	BasePath:         "/api",
+	Version:          "",
+	Host:             "",
+	BasePath:         "",
 	Schemes:          []string{},
-	Title:            "Rumah Belajar Server",
-	Description:      "API definition for Rumah Belajar Server",
+	Title:            "",
+	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
